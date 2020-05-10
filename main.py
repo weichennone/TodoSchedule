345d616aa69cbba42220458721c35adfc1ab69e0
Wei Chen <cweinone@gmail.com>
2020-05-09 23:38:58 -0400

    Todo Schedule v0.2: basic function
    1. add a new task
    2. finish a task
    3. save finished task history each day
    4. add subtask
    5. update the widgets arrangement
    6. provide serial number

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from os import path
+from datetime import datetime
 import json
 
 class Application(tk.Frame):
@@ -8,12 +9,15 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, master)
         self.pack()
         
-        self.tasklist = {}
-        self.serial_num = 0
-        self.finished_list = []
+        self.tasklist = {} # key: serial number, value: (task name, dict for sub tasks) or task name
+        self.finished_list = {}
+        self.parent_task = {}
+        
+        self._serial_num = 0
+        self._idset = set()
         
         self.logpath = "log.log"
-        self.historypath = "history.log"
+#        self.historypath = "history.log"
         self.showstr = ""
         
         self.root = master
@@ -24,53 +28,143 @@ class Application(tk.Frame):
     def createWidgets(self):
     
         self.button_add = tk.Button(self, text="Add", width=8, command=self.add)
-#        self.button_add_to = tk.Button(self, text="Add to", width=8)
+        self.button_add_to = tk.Button(self, text="Add to", width=8, command=self.add_to)
 #        self.button_start = tk.Button(self, text="Start", width=8)
         self.button_finish = tk.Button(self, text="Finish", width=8, command=self.finish)
 #        self.button_delete = tk.Button(self, text="Delete", width=8, command=self.delete)
 #        self.button_exit = tk.Button(self, text="Exit", width=8, command=self.finish)
-        self.button_add.grid(row = 0, column = 0, sticky = tk.W, pady = 2)
-#        self.button_add_to.grid(row = 0, column = 1, sticky = tk.W, pady = 2)
+        self.button_add.grid(row = 0, column = 1, sticky = tk.W, pady = 2)
+        self.button_add_to.grid(row = 0, column = 3, sticky = tk.W, pady = 2)
 #        self.button_start.grid(row = 0, column = 2, sticky = tk.W, pady = 2)
-        self.button_finish.grid(row = 0, column = 1, sticky = tk.W, pady = 2)
+        self.button_finish.grid(row = 0, column = 4, sticky = tk.W, pady = 2)
 #        self.button_delete.grid(row = 0, column = 1, sticky = tk.W, pady = 2)
+
+        self.label_id = tk.Label(self, text="ID:", width=8, justify=tk.RIGHT)
+        self.label_id.grid(row = 1, column = 0, sticky = tk.W, pady = 2)
+        self.entry_id = ttk.Entry(self, width=8)
+        self.entry_id.grid(row = 1, column = 1, sticky = tk.W, pady = 2)
         
-        self.entry = ttk.Entry(self, width=16)
-        self.entry.grid(row = 1, column = 0, columnspan=2, sticky = tk.W, pady = 2)
+        self.label_task = tk.Label(self, text="Task:", width=8, justify=tk.RIGHT)
+        self.label_task.grid(row = 1, column = 2, sticky = tk.W, pady = 2)
+        self.entry_task = ttk.Entry(self, width=16)
+        self.entry_task.grid(row = 1, column = 3, columnspan=2, sticky = tk.W, pady = 2)
         
         self.text = tk.StringVar()
         self.text.set(self.showstr)
-        self.task_display = tk.Label(self, textvariable=self.text)
+        self.task_display = tk.Label(self, textvariable=self.text, justify=tk.LEFT)
         
-        self.task_display.grid(row = 2, column = 0, columnspan=2, sticky = tk.W, pady = 2)
+        self.task_display.grid(row = 2, column = 0, columnspan=5, sticky = tk.W, pady = 2)
         
         self.readlog()
         self.display()
     
+    def _assign_id(self):
+        
+        while str(self._serial_num) in self._idset:
+            self._serial_num += 1
+            self._serial_num %= 1024
+        
+        self._idset.add(str(self._serial_num))
+        return str(self._serial_num)
+        
+    def _collect_id(self, id):
+        self._idset.remove(id)
+    
     # Add a task
     def add(self):
-        taskname = self.entry.get()
-        self.entry.delete(0, 'end')
+        taskname = self.entry_task.get()
+        self.entry_task.delete(0, 'end')
         if taskname != "":
-            self.tasklist[str(self.serial_num)] = taskname
-            self.serial_num += 1
+#            self.serial_num += 1
+            sid = self._assign_id()
+            self.tasklist[sid] = taskname
+#            self.tasklist[str(self.serial_num)] = taskname
+            
+            self.display()
+            
+    # Add a task
+    def add_to(self):
+        id = self.entry_id.get()
+        taskname = self.entry_task.get()
+        self.entry_id.delete(0, 'end')
+        self.entry_task.delete(0, 'end')
+        if taskname != "":
+#            self.serial_num += 1
+            sid = self._assign_id()
+            
+            # if the subtask exists, add a new subtask to the dict
+            if isinstance(self.tasklist[id], list):
+                root_task, subtask_dict = self.tasklist[id]
+                subtask_dict[sid] = taskname
+#                subtask_dict[str(self.serial_num)] = taskname
+            
+            # if the subtask doesn't exist, create a dict and add a new subtask
+            else:
+                root_task = self.tasklist[id]
+                subtask_dict = {sid: taskname}
+#                subtask_dict = {str(self.serial_num): taskname}
+            self.tasklist[id] = [root_task, subtask_dict]
+            
+            # add link from subtask to parent task
+            self.parent_task[sid] = id
+#            self.parent_task[str(self.serial_num)] = id
             
             self.display()
     
     # Finish a task
     def finish(self):
-        id = self.entry.get()
-        self.entry.delete(0, 'end')
-        if id != "":
+        id = self.entry_id.get()
+        self.entry_id.delete(0, 'end')
+        
+        # if finish a parent task
+        if id != "" and id in self.tasklist:
+#            print("root")
+        
+            if isinstance(self.tasklist[id], list):
+                if len(self.tasklist[id][1]) > 0:
+                    return
+                else:
+                    task_item = self.tasklist[id][0]
             
-            try:
-#                id = int(id)
+            else:
                 task_item = self.tasklist[id]
-            except ValueError:
-                return
             
-            self.finished_list.append(task_item)
+            dt = datetime.now()
+            date = "{:4d}{:02d}{:02d}".format(dt.year, dt.month, dt.day)
+            time = "{:2d}:{:02d}:{:02d}".format(dt.hour, dt.minute, dt.second)
+            
+            if date in self.finished_list:
+                self.finished_list[date].append((time, task_item))
+            else:
+                self.finished_list[date] = [(time, task_item)]
+            
             del self.tasklist[id]
+            self._collect_id(id)
+            
+            self.display()
+        
+        # if finish a subtask
+        elif id in self.parent_task and self.parent_task[id] in self.tasklist:
+#            print("sub")
+            
+            pid = self.parent_task[id]
+            root_task, subtask_dict = self.tasklist[pid]
+            task_item = subtask_dict[id]
+            
+            dt = datetime.now()
+            date = "{:4d}{:02d}{:02d}".format(dt.year, dt.month, dt.day)
+            time = "{:2d}:{:02d}:{:02d}".format(dt.hour, dt.minute, dt.second)
+            
+            if date in self.finished_list:
+                self.finished_list[date].append((time, root_task + ": " + task_item))
+            else:
+                self.finished_list[date] = [(time, root_task + ": " + task_item)]
+            print(root_task + ": " + task_item)
+            
+            del subtask_dict[id]
+            del self.parent_task[id]
+            self._collect_id(id)
+            self.tasklist[pid] = [root_task, subtask_dict]
             
             self.display()
     
@@ -82,12 +176,28 @@ class Application(tk.Frame):
     
     # Update the self.task_display
     def display(self):
-        self.showstr = ""
+        self.showstr = "\t".join(["ID", "TODO"])
+        self.showstr += "\n"
+        self.showstr += "\t".join(["----", "--------"])
         for k in self.tasklist:
-            self.showstr += "\t".join([k, self.tasklist[k]])
-            self.showstr += "\n"
-            self.serial_num = max(self.serial_num, int(k))
-        self.serial_num += 1
+            self.showstr += "\n\n"
+            
+            # if the subtask exists
+            if isinstance(self.tasklist[k], list):
+                root_task, subtask_dict = self.tasklist[k]
+                self.showstr += "\t".join([k, root_task])
+                
+                # show subtasks
+                for k2 in subtask_dict:
+                    self.showstr += "\n"
+                    self.showstr += "\t".join([k2, "|_. " + subtask_dict[k2]])
+            
+            # if the subtask doesn't exist
+            else:
+                self.showstr += "\t".join([k, self.tasklist[k]])
+            
+#            self.serial_num = max(self.serial_num, int(k))
+#        self.serial_num += 1
         self.text.set(self.showstr)
             
     # Exit the app
@@ -106,13 +216,25 @@ class Application(tk.Frame):
         if path.exists(self.logpath):
             with open(self.logpath, 'r') as f:
                 self.tasklist = json.load(f)
+#        print(self.tasklist)
+        
+        # re-create parent dict
+        for k in self.tasklist:
+            self._idset.add(k)
+            if isinstance(self.tasklist[k], list):
+                root_task, subtask_dict = self.tasklist[k]
+                for k2 in subtask_dict:
+                    self._idset.add(k2)
+                    self.parent_task[k2] = k
     
     # Append finished tasks to the file
     def writehistory(self):
-        with open(self.historypath, 'a') as f:
-            for history in self.finished_list:
-                f.write(history)
-                f.write('\n')
+        for k in self.finished_list:
+            historypath = "history/" + k + ".log"
+            with open(historypath, 'a') as f:
+                for history in self.finished_list[k]:
+                    time, task = history
+                    f.write(time + '\t\t' + task + '\n')
         
 if __name__ == '__main__':
     app = Application(tk.Tk())
